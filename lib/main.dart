f8f6bb87847e3f5d16d0f65d479aa3fcdfde911c
Olatunji <sirhollyfat@gmail.com>
2019-12-21 12:00:58 +0100

    Create routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:world_app/pages/choose_location.dart';
 import 'package:world_app/pages/home.dart';
+import 'package:world_app/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
   home: Home(),
+  routes: {
+    '/' : (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
 ));
