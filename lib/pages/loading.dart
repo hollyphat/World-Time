9ca0845219a1b15a613e36f424451f5c58147e76
Olatunji <sirhollyfat@gmail.com>
2019-12-21 11:52:33 +0100

    Create loading layout

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,6 +8,8 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Text("Loading Screen"),
+    );
   }
 }
